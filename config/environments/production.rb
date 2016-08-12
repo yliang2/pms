c1a8a75ecbb69d0715bb205ef8278f9e717a347a
yliang2 <yliang2@my.hpu.edu>
2016-08-11 22:33:16 -0400

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  onfig.public_file_server.enabled = true
+  config.public_file_server.enabled = true
 end
